9aac058d129710ff0d05a56f73dbdea4d3dd6dc7
Aly <asecure05@gmail.com>
2022-09-25 17:24:40 +0200

    Setting up simple navigation and basic Hilt setup.

---
+++ app/src/main/java/com/example/pokedex/PokemonAppNavHost.kt
@@ -0,0 +1,46 @@
+package com.example.pokedex
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+
+@Composable
+fun PokemonAppNavHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "pokemon_list_screen"
+    ) {
+        composable("pokemon_list_screen") {
+            // TODO: call the screen composable
+        }
+        composable(
+            "pokemon_detail_screen/{pokemonName}/{dominantColor}",
+            arguments = listOf(
+                navArgument("pokemonName") {
+                    type = NavType.StringType
+                    nullable = false
+                },
+                navArgument("dominantColor") {
+                    type = NavType.IntType
+                    nullable = false
+                }
+            )
+        ) {
+            val pokemonName = remember {
+                it.arguments?.getString("pokemonName")
+            }
+            val dominantColor = remember {
+                val color = it.arguments?.getInt("dominantColor")
+                color?.let {
+                    Color(color)
+                } ?: Color.White
+            }
+            // TODO: call the screen composable
+        }
+    }
+}
